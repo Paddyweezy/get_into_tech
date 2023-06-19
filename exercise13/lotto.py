f3dff9ef54d58b82441abbe6492860de5fcafa64
PaddyWeezy <Patrickwhelan1@hotmail.co.uk>
2023-06-19 19:48:08 +0100

    updates to e13

--- exercise13/lotto.py
+++ exercise13/lotto.py
@@ -4,14 +4,17 @@
 
 import random
 
-lotto_numbers = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18',
-                 '19', '20', '21', '22', '23', '24', '25', '26', '27', '29', '30', '31', '32', '33', '34', '35', '36',
-                 '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50')
-random_six = random.sample(lotto_numbers, 6)
+# lotto_numbers = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18',
+#                  '19', '20', '21', '22', '23', '24', '25', '26', '27', '29', '30', '31', '32', '33', '34', '35', '36',
+#                  '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50')
+
+random_six = random.sample(range(1, 51), 6)
 print(random_six)
+
+# this is all you need
 
 # quicker way to achieve the same thing - work smarter not harder.
 
-lotto_numbers2 = list(range(1, 51)) #this create a data range from 1 to 50
-random_six2 = random.sample(lotto_numbers2, 6) #
-print(random_six2)
+# lotto_numbers2 = list(range(1, 51)) #this create a data range from 1 to 50
+# random_six2 = random.sample(lotto_numbers2, 6) #
+# print(random_six2)
--- exercise13/what_is_going_on_here.py
+++ exercise13/what_is_going_on_here.py
@@ -1,12 +1,16 @@
 tup = "hello"
 print(len(tup))
+print(type(tup))
 # tup is defined as the sting "hello" print(len(tup)) is printing how many characters are in the string. i.e hello = 5
 # characters.
 
 tup = "hello",
 print(len(tup))
-# the itroduction of a comma after the string makes the print function count the amount of strings there are
+print(type(tup))
+# the itroduction of a comma after the string makes it a tuple,  the print function count the amount of strings there
+# are
 
 tup = "hello", "goodbye"
 print(len(tup))
+print(type(tup))
 # by putting a second string separated by a comma results in the number 2 being printed.
